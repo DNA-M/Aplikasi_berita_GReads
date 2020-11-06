b00605c4905c00805923a5d6bcbac8d31b729c56
aslamakiki <aslama.kiki22@gmail.com>
2020-11-06 18:41:13 +0700

    membuat navbar dan icon search view

--- app/src/main/java/com/example/greads/MainActivity.java
+++ app/src/main/java/com/example/greads/MainActivity.java
@@ -1,30 +1,28 @@
 package com.example.greads;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    public static final  String EXTRA_MESSAGE = "com.example.android.greads.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.example.android.greads.extra.MESSAGE";
     TabLayout tabLayout;
     ViewPager viewPager;
 
@@ -33,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_navbar);
 
-        androidx.appcompat.widget.Toolbar toolbar =
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar)
                 findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -53,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        //NAVBAR
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer != null) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
     }
 
     @Override
@@ -63,21 +88,33 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Handle navigation view item clicks here.
         switch (item.getItemId()) {
-
-            case R.id.setting:
-                Intent setting = new Intent(MainActivity.this, Setting.class);
-                startActivity(setting);
-                return true;
-            case R.id.about:
+            case R.id.nav_about:
                 Intent about = new Intent(MainActivity.this, About_Page.class);
                 startActivity(about);
                 return true;
+            case R.id.nav_setting:
+                Intent setting = new Intent(MainActivity.this, Setting.class);
+                startActivity(setting);
+                return true;
+            case R.id.nav_daynight:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_daynight));
+                return true;
             default:
-                return super.onOptionsItemSelected(item);
+                return false;
         }
-
     }
 
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
 }
